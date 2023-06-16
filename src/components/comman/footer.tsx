44ed5a163cdd0e5ed02c60f50619b0b29f38525b
NisharMultani <nisharmultani7@gmail.com>
2023-06-16 19:06:43 +0530

    feat: add comman  component

---
+++ src/components/comman/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Footer = () => {
+    return (
+
+        <footer>
+            <h2 className="bg-gray-600 text-red-600">footer</h2>
+        </footer>
+
+    )
+}
+
+export default Footer
---
+++ src/components/comman/masterHeader.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+const MasterHeader = () => {
+
+    return (
+        <>
+            <header>
+                <h1>master hero HEader</h1>
+            </header>
+        </>
+    )
+}
+
+export default MasterHeader
